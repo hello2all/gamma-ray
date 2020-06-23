e666d4e048aa2720507c1220df626fe037296b78
Ted <hellot2@hotmail.com>
2020-06-24 01:48:05 +0800

    setup precompiled header files

--- run-build.sh
+++ run-build.sh
@@ -1,5 +1,7 @@
-rm -rf build
-mkdir build && cd build
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+cd build
 cmake -GNinja -DCMAKE_BUILD_TYPE=Debug .. 
 ninja
 LD_LIBRARY_PATH=/usr/local/lib ./main
