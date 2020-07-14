0452442c28bc6d33af2b219add1415cdb556686a
Ted <hellot2@hotmail.com>
2020-07-15 00:57:46 +0800

    models split header and source

--- include/models.h
+++ include/models.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <iostream>
 #include "json.hpp"
-#include "util.h"
+#include "Poco/DateTime.h"
+#include "Poco/DateTimeParser.h"
+#include "Poco/DateTimeFormat.h"
 
 using json = nlohmann::json;
 
 namespace Models
 {
+  // === ENUMS ===
   enum class Exchange
   {
     bitmex
@@ -23,7 +26,17 @@ namespace Models
     Top
   };
 
-  class MarketQuote
+  // === FUNCTIONS ===
+  Poco::DateTime iso8601_to_datetime(const std::string &s);
+
+  // === CLASSES ===
+  class Timestamped
+  {
+  public:
+    Poco::DateTime time;
+  };
+
+  class MarketQuote : public Timestamped
   {
   public:
     double askPrice;
@@ -31,26 +44,10 @@ namespace Models
     double bidPrice;
     double bidSize;
     std::string symbol;
-    Poco::DateTime time;
 
-    MarketQuote()
-    {
-    }
-
-    MarketQuote(json &q)
-    {
-      this->askPrice = q["askPrice"].get<double>();
-      this->askSize = q["askSize"].get<double>();
-      this->bidPrice = q["bidPrice"].get<double>();
-      this->bidSize = q["bidSize"].get<double>();
-      this->symbol = q["symbol"].get<std::string>();
-      this->time = util::datetime::parse_iso_8601_string(q["timestamp"].get<std::string>());
-    }
-
-    MarketQuote(double askPrice, double askSize, double bidPrice, double bidSize, const std::string &symbol, Poco::DateTime time)
-        : askPrice(askPrice), askSize(askSize), bidPrice(bidPrice), bidSize(bidSize), symbol(symbol), time(time)
-    {
-    }
+    MarketQuote();
+    MarketQuote(json &q);
+    MarketQuote(double askPrice, double askSize, double bidPrice, double bidSize, const std::string &symbol, Poco::DateTime time);
   };
 
   enum class Side
@@ -66,10 +63,16 @@ namespace Models
     double size;
     Side side;
 
-    Quote(double price, double size, Side side)
-        : price(price), size(size), side(side)
-    {
-    }
+    Quote(double price, double size, Side side);
+  };
+
+  class TwoSidedQuote : public Timestamped
+  {
+  public:
+    Quote bid;
+    Quote ask;
+
+    TwoSidedQuote(Quote bid, Quote ask, Poco::DateTime time);
   };
 
   class QuoteOrder
@@ -77,26 +80,17 @@ namespace Models
   public:
     Quote quote;
     std::string orderId;
-    QuoteOrder(Quote &quote, const std::string &orderId)
-        : quote(quote), orderId(orderId)
-    {
-    }
+
+    QuoteOrder(Quote &quote, const std::string &orderId);
   };
 
-  class FairValue
+  class FairValue : public Timestamped
   {
   public:
     double price;
-    Poco::DateTime time;
 
-    FairValue()
-    {
-    }
-
-    FairValue(double price, Poco::DateTime time)
-        : price(price), time(time)
-    {
-    }
+    FairValue();
+    FairValue(double price, Poco::DateTime time);
   };
 
   class QuotingParameters
@@ -110,9 +104,6 @@ namespace Models
     double trades_per_minute;
     double trade_rate_seconds;
 
-    QuotingParameters(double width, double size, double target_base_position, double position_divergence, double skew_factor, double trades_per_minute, double trade_rate_seconds)
-        : width(width), size(size), target_base_position(target_base_position), position_divergence(position_divergence), skew_factor(skew_factor), trades_per_minute(trades_per_minute), trade_rate_seconds(trade_rate_seconds)
-    {
-    }
+    QuotingParameters(double width, double size, double target_base_position, double position_divergence, double skew_factor, double trades_per_minute, double trade_rate_seconds);
   };
 } // namespace Models
---
+++ src/models.cpp
@@ -0,0 +1,63 @@
+#include "models.h"
+
+namespace Models
+{
+  Poco::DateTime iso8601_to_datetime(const std::string &s)
+  {
+    Poco::DateTime dt;
+    int tzd;
+    Poco::DateTimeParser::parse(Poco::DateTimeFormat::ISO8601_FORMAT, s, dt, tzd);
+    return dt;
+  }
+
+  MarketQuote::MarketQuote()
+  {
+  }
+
+  MarketQuote::MarketQuote(json &q)
+  {
+    this->askPrice = q["askPrice"].get<double>();
+    this->askSize = q["askSize"].get<double>();
+    this->bidPrice = q["bidPrice"].get<double>();
+    this->bidSize = q["bidSize"].get<double>();
+    this->symbol = q["symbol"].get<std::string>();
+    this->time = iso8601_to_datetime(q["timestamp"].get<std::string>());
+  }
+
+  MarketQuote::MarketQuote(double askPrice, double askSize, double bidPrice, double bidSize, const std::string &symbol, Poco::DateTime time)
+      : askPrice(askPrice), askSize(askSize), bidPrice(bidPrice), bidSize(bidSize), symbol(symbol)
+  {
+    this->time = time;
+  }
+
+  Quote::Quote(double price, double size, Side side)
+      : price(price), size(size), side(side)
+  {
+  }
+
+  TwoSidedQuote::TwoSidedQuote(Quote bid, Quote ask, Poco::DateTime time)
+      : bid(bid), ask(ask)
+  {
+    this->time = time;
+  }
+
+  QuoteOrder::QuoteOrder(Quote &quote, const std::string &orderId)
+      : quote(quote), orderId(orderId)
+  {
+  }
+
+  FairValue::FairValue()
+  {
+  }
+
+  FairValue::FairValue(double price, Poco::DateTime time)
+      : price(price)
+  {
+    this->time = time;
+  }
+
+  QuotingParameters::QuotingParameters(double width, double size, double target_base_position, double position_divergence, double skew_factor, double trades_per_minute, double trade_rate_seconds)
+      : width(width), size(size), target_base_position(target_base_position), position_divergence(position_divergence), skew_factor(skew_factor), trades_per_minute(trades_per_minute), trade_rate_seconds(trade_rate_seconds)
+  {
+  }
+} // namespace Models
