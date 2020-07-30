6ab2afd5e1c6b5c08f8a0fb5234700c582763585
Ted <hellot2@hotmail.com>
2020-07-30 14:27:18 +0800

    seperate debug / release build

--- run-release.sh
+++ run-release.sh
@@ -1,7 +1,7 @@
-if [ ! -d "build" ]; then
-  mkdir build
+if [ ! -d "release" ]; then
+  mkdir release
 fi
-cd build
+cd release
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release .. || exit 1
 ninja || exit 1
 LD_LIBRARY_PATH=/usr/local/lib ./main
