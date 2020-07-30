cbc9b7de18330a6e0d0988589699f29d45ad2937
Ted <hellot2@hotmail.com>
2020-07-31 00:33:23 +0800

    modify upload script

--- upload.sh
+++ upload.sh
@@ -1,1 +1,14 @@
-scp ./build/main ubuntu@54.195.186.38:~
+DEBUG_EXE=./debug/main
+RELEASE_EXE=./release/main
+REMOTE_ADDR=54.195.186.38
+REMOTE_USER=ubuntu
+
+if test -f "$DEBUG_EXE"; then
+echo "uploading debug executable..."
+scp "$DEBUG_EXE" $REMOTE_USER@$REMOTE_ADDR:~/debug
+fi
+
+if test -f "$RELEASE_EXE"; then
+echo "uploading release executable..."
+scp "$RELEASE_EXE" $REMOTE_USER@$REMOTE_ADDR:~/release
+fi
