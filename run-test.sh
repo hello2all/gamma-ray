15a42973445a526d42e73a6ebe9060b22ecc1001
Ted <hellot2@hotmail.com>
2020-08-27 01:57:42 +0800

    update run test script

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,7 @@
-if [ ! -d "build" ]; then
-  mkdir build
+if [ ! -d "debug" ]; then
+  mkdir debug
 fi
-cd build
+cd debug
 cmake -GNinja -DCMAKE_BUILD_TYPE=Debug .. || exit 1
 ninja || exit 1
 LD_LIBRARY_PATH=/usr/local/lib ./unit_tests
